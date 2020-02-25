03009e7912b96198d3d77a44dcd2d37501d5ea00
Noa Rahamim <nr8112@bs.amalnet.k12.il>
2020-02-25 14:18:00 +0200

    1

---
+++ app/src/main/java/com/example/exx1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.exx1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b = findViewById(R.id.b);
+    }
+
+    public void uo(View view) {
+        b.setText("Oh, yea, I’ve been clicked!");
+    }
+}
